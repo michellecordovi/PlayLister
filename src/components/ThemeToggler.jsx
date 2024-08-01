9fadd59a4b3597e2703630349ef824d4b7ceccd8
MichelleCordovi <michellecordovi@gmail.com>
2024-08-01 18:26:17 -0400

    create logic for toggle button on click

--- src/components/ThemeToggler.jsx
+++ src/components/ThemeToggler.jsx
@@ -1,9 +1,28 @@
+import {useState, useEffect, useRef} from 'react';
+
 function ThemeToggler() {
+    const [mode, setMode] = useState('dark');
+    const toggleRef = useRef(null);
+
+    useEffect(()=>{
+        if(mode === 'dark') {
+            toggleRef.current.style.justifyContent="flex-end";
+        } else if(mode === 'light'){
+            toggleRef.current.style.justifyContent="flex-start";
+        }
+    }, [mode])
+
+    function handleToggleClick() {
+        setMode((prevMode) =>
+            (prevMode === 'dark' ?'light' : 'dark')
+        )
+    }
+
     return (
         <div className="theme-adjuster">
             <svg id="light-mode-icon" xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px" fill="#fff"><path d="M480-340q58 0 99-41t41-99q0-58-41-99t-99-41q-58 0-99 41t-41 99q0 58 41 99t99 41Zm0 60q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-450H40v-60h160v60Zm720 0H760v-60h160v60ZM450-760v-160h60v160h-60Zm0 720v-160h60v160h-60ZM262-658l-100-97 43-44 96 100-39 41Zm494 496-98-100 41-41 99 98-42 43Zm-99-537 98-99 44 42-99 98-43-41ZM162-205l99-98 42 42-98 99-43-43Zm318-275Z"/></svg>
             
-            <div id="theme-toggler">
+            <div className="theme-toggler" onClick={handleToggleClick} ref={toggleRef}>
                 <div id="toggle-ball"></div>
             </div>
 
