ba98cfa45a1798a67ca54cbac30bc338ed24f283
MichelleCordovi <michellecordovi@gmail.com>
2024-08-03 15:15:55 -0400

    create styling for resulted section and tracks

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,54 @@ function App() {
             artist: "Chappel Roan",
             album: "The Rise and Fall of a Midwest Princess",
             src: 'https://media.pitchfork.com/photos/64ff1676931354660ba71d8b/master/w_1280%2Cc_limit/Chappell-Roan-Princess.jpg'
+        },
+
+        {
+            id: 2,
+            name: "Born Under Punches",
+            artist: "Talking Heads",
+            album: "Remain In Light",
+            src: "https://upload.wikimedia.org/wikipedia/en/2/2d/TalkingHeadsRemaininLight.jpg"
+        },
+
+        {
+            id: 2,
+            name: "Born Under Punches",
+            artist: "Talking Heads",
+            album: "Remain In Light",
+            src: "https://upload.wikimedia.org/wikipedia/en/2/2d/TalkingHeadsRemaininLight.jpg"
+        },
+
+        {
+            id: 2,
+            name: "Born Under Punches",
+            artist: "Talking Heads",
+            album: "Remain In Light",
+            src: "https://upload.wikimedia.org/wikipedia/en/2/2d/TalkingHeadsRemaininLight.jpg"
+        },
+
+        {
+            id: 2,
+            name: "Born Under Punches",
+            artist: "Talking Heads",
+            album: "Remain In Light",
+            src: "https://upload.wikimedia.org/wikipedia/en/2/2d/TalkingHeadsRemaininLight.jpg"
+        },
+
+        {
+            id: 2,
+            name: "Born Under Punches",
+            artist: "Talking Heads",
+            album: "Remain In Light",
+            src: "https://upload.wikimedia.org/wikipedia/en/2/2d/TalkingHeadsRemaininLight.jpg"
+        },
+
+        {
+            id: 2,
+            name: "Born Under Punches",
+            artist: "Talking Heads",
+            album: "Remain In Light",
+            src: "https://upload.wikimedia.org/wikipedia/en/2/2d/TalkingHeadsRemaininLight.jpg"
         }
     ]
 
--- src/components/Tracklist.jsx
+++ src/components/Tracklist.jsx
@@ -3,7 +3,7 @@ import Track from "./Track";
 
 function Tracklist({tracks}){
     return (
-        <div className="Tracklist">
+        <div className="tracklist">
            {tracks.map((track)=> (
             <Track key={track.id} artist={track.artist} song={track.name} album={track.album} src={track.src}/>
            ))}
