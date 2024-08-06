8dd7cdaff283e227162cb533b4c0b5fee20918c6
MichelleCordovi <michellecordovi@gmail.com>
2024-08-06 15:25:56 -0400

    create fetch function to search for tracks from input

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,45 @@
+import { useState, useEffect } from "react";
+
 function SearchBar(){
+    const [searchInput, setSearchInput] = useState('');
+    const [searchResults, setSearchResults] = useState([])
+    const baseUrl = 'https://api.spotify.com/v1/search/'
+
+    const handleSearchClick = async (event) => {
+        event.preventDefault();
+    
+        const url = `${baseUrl}?q=${encodeURIComponent(searchInput)}&type=track&limit=10`;
+    
+        const response = await fetch(url, {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${localStorage.access_token}`,
+            'Content-Type': 'application/json'
+          }
+        });
+    
+        if (response.ok) {
+          const data = await response.json();
+          setSearchResults(data.tracks.items); // Set the search results to the track items
+        } else {
+          console.error('Search request failed', response.status, response.statusText);
+        }
+      };
+    
+      useEffect(() => {
+        console.log(searchResults);
+      }, [searchResults]);
+
+    function handleInputChange(event) {
+        setSearchInput(event.target.value);
+    }
+
     return (
-        <form>
-            <input id="searchInput" type="text" placeholder="Search Artist, Song, Album, etc..."/>
+        <form onSubmit={handleSearchClick}>
+            <input id="searchInput" type="text" placeholder="Search Artist, Song, Album, etc..." value={searchInput} onChange={handleInputChange}/>
             <input id="searchInputSubmit" type="submit" value="Search"/>
         </form>
     )
 }
 
-export default SearchBar
+export default SearchBar;
